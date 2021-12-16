e5aacb87127848f1bc5226d0205c25e6dcdfa1a5
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-17 00:12:53 +0200

    Filter c-t refactoring

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,29 +1,23 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Filter.module.css';
 
-class Filter extends Component {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <div className={s.wrapper}>
-        <label className={s.label} htmlFor="filter">
-          Find contacts by name
-        </label>
-        <input
-          className={s.input}
-          id="filter"
-          value={value}
-          onChange={onChange}
-        ></input>
-      </div>
-    );
-  }
+export default function Filter({ value, onChange }) {
+  return (
+    <div className={s.wrapper}>
+      <label className={s.label} htmlFor="filter">
+        Find contacts by name
+      </label>
+      <input
+        className={s.input}
+        id="filter"
+        value={value}
+        onChange={onChange}
+      ></input>
+    </div>
+  );
 }
 
-export default Filter;
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
