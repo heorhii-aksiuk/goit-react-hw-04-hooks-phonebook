5ffa973017f3bb1fa9f1e017127cefb98d5c2d24
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-17 00:11:20 +0200

    Contacts c-t refactoring

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,42 +1,36 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Contacts.module.css';
 
-class Contacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      }),
-    ),
-  };
-
-  render() {
-    const { contacts, onRemoveClick } = this.props;
-    return (
-      <ul>
-        {contacts.map(({ id, name, number }) => {
-          return (
-            <li className={s.item} key={id} name={name}>
-              <p className={s.paragraph}>
-                <span>{name}</span>
-                <span>{number}</span>
-              </p>
-              <button
-                className={s.button}
-                onClick={() => onRemoveClick(id)}
-                type="button"
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
+export default function Contacts({ contacts, onRemoveClick }) {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li className={s.item} key={id} name={name}>
+            <p className={s.paragraph}>
+              <span>{name}</span>
+              <span>{number}</span>
+            </p>
+            <button
+              className={s.button}
+              onClick={() => onRemoveClick(id)}
+              type="button"
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+};
